5d02465337655cf81413cabf23b9e8aaa70a913a
D1saster20 <yuriizhukevych@gmail.com>
2022-12-13 18:32:57 +0200

    1

---
+++ main.js
@@ -0,0 +1,22 @@
+
+owu = 'owu'
+com = 'com'
+ua = 'ua'
+console.log(owu,com,ua)
+firstname = 'yurii'
+middlename ='zhukevych'
+lastname = 'yaroslavovych'
+console.log(firstname, middlename, lastname)
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+
+
+console.log(prompt('Яке у вас імя?',''))
+console.log(prompt('Яке вас по батькові ?',''))
+console.log(prompt('Скільки вам років ?',''))
+
+
